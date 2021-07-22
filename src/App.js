4c4b253ef91dc49bb9208c0d070e3fe9c5b6d7ad
Maksym Plevnytskyi <76871115+Berfeks@users.noreply.github.com>
2021-07-22 10:42:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { Preloader } from "./components/Preloader";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
